f1cf8135983ef081bcc77a0ce17d29a9ad61111d
suppportksk-crypto <suppportksk@gmail.com>
2026-07-13 17:10:34 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Security Assessment Platform</h1>
+      <p>This platform is for authorized security testing only.</p>
+    </div>
+  );
+}
